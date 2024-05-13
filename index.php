066d097af5c4c5917e5b82c871081e206f5cde36
kishan <joeko@hotmail.com>
2024-05-13 13:03:51 +0200

    basis pagina

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+    <html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Kishan">
+        <meta name="keywords" content="">
+        <title>Pixelground</title>
+        <link rel="stylesheet" type="text/css" href="Css/styl.css">
+    </head>
+    <body>
+    <header>
+
+     </header>
+<main>
+    <section>
+
+    </section>
+</main>
+<footer>
+
+</footer>
+    </body>
+    </html>
---
+++ loginpage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+    <html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Kishan">
+        <meta name="keywords" content="">
+        <title>Pixelground</title>
+        <link rel="stylesheet" type="text/css" href="Css/styl.css">
+    </head>
+    <body>
+    <header>
+
+     </header>
+<main>
+    <section class="flex_register">
+        <article class="register">
+        <h2>Register</h2>
+            <form action="loginpage.php" method="post">
+                <input type="text" name="Naam" placeholder="Naam">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="Gebruikersnaam" placeholder="Gebruikersnaam">
+                <input type="password" name="Wachtwoord" placeholder="Wachtwoord">
+                <input type="submit" value="Register">
+            </form>
+        </article>
+    </section>
+</main>
+<footer>
+
+</footer>
+    </body>
+    </html>
